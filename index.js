a6bc3c07a64c20725da6af3b2d5b45f819c83ead
qaf1 <gmail@qaf.ae>
2025-01-27 21:42:29 +0530

    chgange

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { GoogleAuth } = require('google-auth-library');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Path to your service account JSON file
 const keyPath = path.join(__dirname, 'serviceAccountKey.json');
